8d0fc250a631ee5b378a360bd078477ca4062821
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-08-05 18:47:25 +0100

    adding data source object to display artist from stub data

---
+++ itunesClient/SearchResultsDataSource.swift
@@ -0,0 +1,45 @@
+//
+//  SearchResultsDataSource.swift
+//  itunesClient
+//
+//  Created by Leke Abolade on 05/08/2017.
+//  Copyright © 2017 Leke Abolade. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsDataSource: NSObject, UITableViewDataSource {
+    
+    private var data = [Artist]()
+    
+    override init() {
+        super.init()
+    }
+    
+    func update(with artists: [Artist]) {
+        data = artists
+    }
+    
+    // MARK: - Data Source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell", for: indexPath)
+        
+        let artist = data[indexPath.row]
+        cell.textLabel?.text = artist.name
+        
+        return cell
+    }
+    
+    
+    
+
+}
