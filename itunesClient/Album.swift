a6f2cfc10b8f4bc8afc5987fae67f2f18eb434da
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-08-05 16:59:44 +0100

    adding album class

--- itunesClient/Album.swift
+++ itunesClient/Album.swift
@@ -9,5 +9,28 @@
 import Foundation
 
 class Album {
+    let id: Int
+    let artistName: String
+    let name: String
+    let censoredName: String
+    let artworkURL: String
+    let isExplicit: Bool
+    let numberOfTracks: Int
+    let releaseDate: Date
+    let primaryGenre: Genre
+    var songs = [Song]()
+    
+    init(id: Int, artistName: String, name: String, censoredName: String, artworkUrl: String, isExplicit: Bool, numberOfTracks: Int, releaseDate: Date, primaryGenre: Genre) {
+        self.id = id
+        self.artistName = artistName
+        self.name = name
+        self.censoredName = censoredName
+        self.artworkURL = artworkUrl
+        self.isExplicit = isExplicit
+        self.numberOfTracks = numberOfTracks
+        self.releaseDate = releaseDate
+        self.primaryGenre = primaryGenre
+    }
+    
     
 }
---
+++ itunesClient/Song.swift
@@ -0,0 +1,13 @@
+//
+//  Song.swift
+//  itunesClient
+//
+//  Created by Leke Abolade on 05/08/2017.
+//  Copyright © 2017 Leke Abolade. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    
+}
