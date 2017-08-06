f2afa743394ad7efb3ddbcf1a22ec4aa9aef9f44
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-08-06 15:03:48 +0100

    creating search endpoint for itunes api

--- itunesClient/ViewController.swift
+++ itunesClient/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let endpoint = Itunes.search(term: "kendrick lamar", media: .music)
+        print(endpoint.request)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
